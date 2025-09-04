7799b0868ca3cbf9e582b9f790fe68d22d51d5bb
Ziloden <ziloden@pm.me>
2025-09-03 17:02:45 -0700

    Add App launcher to winbox

--- install_scripts/install_winbox.sh
+++ install_scripts/install_winbox.sh
@@ -3,6 +3,16 @@
 # Download from https://mikrotik.com/download
 # Assumes it's called WinBox_Linux.zip
 
-mkdir ~/.local/share/Winbox
-unzip WinBox_Linux.zip -d ~/.local/share/Winbox
-ln -s ~/.local/share/Winbox/WinBox ~/.local/bin/WinBox
+mkdir /usr/local/share/Winbox
+unzip WinBox_Linux.zip -d /usr/local/share/Winbox
+ln -s /usr/local/share/Winbox/WinBox /usr/local/bin/winbox
+
+cat <<EOF > /usr/share/applications/winbox.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Name=WinBox
+Exec=/usr/local/bin/winbox
+Icon=/usr/local/bin/winbox
+Type=Application
+Categories=Development;
+EOF
